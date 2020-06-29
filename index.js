5bb7758d06015408fa6430d0df8ee09ee37a9d26
Mandiiiiiii <mandyventurex7@gmail.com>
2020-06-29 09:41:03 -0400

    Finished!

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
 const github = require('./lib/github');
+const repo = require('./lib/repo');
 
 clear();
 
@@ -18,14 +21,53 @@ if (files.directoryExists('.git')) {
     process.exit();
 }
 
+const getGithubToken = async () => {
+    // Fetch token from config store
+    let token = github.getStoredGithubToken();
+    if (token) {
+        return token;
+    }
+
+    // No token found, use credentials to access GitHub account
+    token = await github.getPersonalAccessToken();
+    return token;
+}
+
 const run = async () => {
     // const credentials = await inquirer.askGithubCredentials();
     // console.log(credentials);
 
-    let token = github.getStoredGithubToken();
-    if (!token) {
-        token = await github.getPersonalAccessToken();
+    // let token = github.getStoredGithubToken();
+    // if (!token) {
+    //     token = await github.getPersonalAccessToken();
+    // }
+
+    try {
+        // Retrieve & Set Authentication Token
+        const token = await getGithubToken();
+        github.githubAuth(token);
+
+        // Create remote repository
+        const url = await repo.createRemoteRepo();
+
+        // Set up local repository and push to remote
+        await repo.setupRepo(url);
+
+        console.log(chalk.green('All done!'));
+    } catch (err) {
+        if (err) {
+            switch (err.status) {
+                case 401:
+                    console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+                    break;
+                case 422:
+                    console.log(chalk.red('There is already a remote repository or token with the same name'));
+                    break;
+                default:
+                    console.log(chalk.red(err));
+            }
+        }
     }
-}
+};
 
 run();
