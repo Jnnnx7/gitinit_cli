52edd944c01648ce0a63c2309247b18bb90f5c90
Mandiiiiiii <mandyventurex7@gmail.com>
2020-06-29 08:51:27 -0400

    First commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
+const github = require('./lib/github');
 
 clear();
 
@@ -18,8 +19,13 @@ if (files.directoryExists('.git')) {
 }
 
 const run = async () => {
-    const credentials = await inquirer.askGithubCredentials();
-    console.log(credentials);
+    // const credentials = await inquirer.askGithubCredentials();
+    // console.log(credentials);
+
+    let token = github.getStoredGithubToken();
+    if (!token) {
+        token = await github.getPersonalAccessToken();
+    }
 }
 
 run();
